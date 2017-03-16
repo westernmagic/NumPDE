f66aba18a9d20797c58616cbc33c93dd31bc6f03
Michal Sudwoj <mswoj61@gmail.com>
2017-03-16 13:38:10 +0100

    Series 2 handout

---
+++ series2/1d-linFEM/plot_fem.py
@@ -0,0 +1,14 @@
+import numpy, pylab
+u = numpy.loadtxt("u_wo_bc_fem.txt")
+x = numpy.loadtxt("x_wo_bc_fem.txt")
+pylab.plot(x, u, '-o', label='$u(x)$')
+pylab.plot(x, numpy.sin(x), label='$\\sin(x)$')
+pylab.legend()
+pylab.show()
+
+ubc = numpy.loadtxt("u_w_bc_fem.txt")
+xbc = numpy.loadtxt("x_w_bc_fem.txt")
+pylab.plot(xbc, ubc, '-o', label='$u(x)$')
+pylab.plot(xbc, numpy.cos(x)+ 0.5, label='$\\cos(x)+\\frac{1}{2}$')
+pylab.legend()
+pylab.show()
