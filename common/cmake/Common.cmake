98f2a06f670c50933d07b0e606819d6ddd02af49
Michal Sudwoj <mswoj61@gmail.com>
2017-03-24 14:54:06 +0100

    Added travis configuration.

--- common/cmake/Common.cmake
+++ common/cmake/Common.cmake
@@ -1,12 +1,15 @@
-list (APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
+if (NOT DEFINED GUARD_${CMAKE_CURRENT_LIST_FILE})
+	set (GUARD_${CMAKE_CURRENT_LIST_FILE} TRUE)
+	list (APPEND CMAKE_MODULE_PATH ${CMAKE_CURRENT_LIST_DIR})
 
-include (CommonHeader)
-include (Settings)
+	include (CommonHeader)
+	include (Settings)
 
-include (MyEigen)
-include (MyLibIGL)
-include (MyThreads)
-include (MyGTest)
+	include (MyEigen)
+	include (MyLibIGL)
+	include (MyThreads)
+	include (MyGTest)
 
-include (Submission)
+	include (Submission)
+endif (NOT DEFINED GUARD_${CMAKE_CURRENT_LIST_FILE})
 
