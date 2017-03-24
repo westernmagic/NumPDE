ea21e7c83f3a29faa7c60256033a301c487a96d2
Michal Sudwoj <mswoj61@gmail.com>
2017-03-24 22:14:29 +0100

    Hint Eigen3 location

--- common/cmake/MyEigen.cmake
+++ common/cmake/MyEigen.cmake
@@ -1,2 +1,4 @@
-find_package (Eigen3 3.3 REQUIRED NO_MODULE PATHS ${PROJECT_ROOT_DIR}/external/eigen)
+set (Eigen3_ROOT ${PROJECT_ROOT}/external/eigen/)
+
+find_package (Eigen3 3.3 REQUIRED NO_MODULE)
 
