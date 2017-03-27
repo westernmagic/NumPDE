f3af80516f632f4c3fa75d70fae28af3dc073b26
Michal Sudwoj <mswoj61@gmail.com>
2017-03-27 02:55:09 +0200

    Copied solutions to series 2g & 2g from warmup
    Slight modification of function arguments was needed.

--- series2/2d-linFEM/stiffness_matrix_assembly.hpp
+++ series2/2d-linFEM/stiffness_matrix_assembly.hpp
@@ -33,7 +33,26 @@ void assembleStiffnessMatrix(Matrix& A, const Eigen::MatrixXd& vertices,
     std::vector<Triplet> triplets;
 
     triplets.reserve(numberOfElements * 3 * 3);
-// (write your solution here)
+	// (write your solution here)
+	//// NPDE_START_TEMPLATE
+    for (int i = 0; i < numberOfElements; ++i) {
+        auto& indexSet = triangles.row(i);
+
+        const auto& a = vertices.row(indexSet(0));
+        const auto& b = vertices.row(indexSet(1));
+        const auto& c = vertices.row(indexSet(2));
+
+        Eigen::Matrix3d stiffnessMatrix;
+        computeStiffnessMatrix(stiffnessMatrix, a, b, c, sigma, r);
+
+        for (int n = 0; n < 3; ++n) {
+            for (int m = 0; m < 3; ++m) {
+                auto triplet = Triplet(indexSet(n), indexSet(m), stiffnessMatrix(n, m));
+                triplets.push_back(triplet);
+            }
+        }
+    }
+    //// NPDE_END_TEMPLATE
     A.setFromTriplets(triplets.begin(), triplets.end());
 }
 //----------------AssembleMatrixEnd----------------
