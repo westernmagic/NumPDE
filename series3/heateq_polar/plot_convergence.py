11c8e796b210ad3166d8a701d7358a8fb2d9da23
Michal Sudwoj <mswoj61@gmail.com>
2017-05-02 11:48:48 +0200

    Fix order of arguments in plot_convergence.py

--- series3/heateq_polar/plot_convergence.py
+++ series3/heateq_polar/plot_convergence.py
@@ -5,7 +5,7 @@ import matplotlib.pyplot as plt
 errors = loadtxt('errors.txt')
 resolutions = loadtxt('resolutions.txt')
 
-poly = polyfit(log(errors),log(resolutions),1)
+poly = polyfit(log(resolutions),log(errors),1)
 
 plt.loglog(resolutions, errors, '-o', label='error')
 plt.loglog(resolutions, exp(poly[1])*resolutions**poly[0], '--', label='$\\mathcal{O}(N^{%.2f})$' % poly[0])
